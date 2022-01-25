53dcabc26c5dc9b899f9b69fb0e249eb176c2352
mpolacek1 <matejpolacek1@gmail.com>
2022-01-25 16:12:53 +0100

    added controllers, dynamic settings in UI

---
+++ ExperimentalProbability.UI/Controllers/DiceController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExperimentalProbability.UI.Properties;
+using ExperimentalProbability.Calculations.Models;
+using ExperimentalProbability.Calculations.Types;
+using System.Windows;
+
+namespace ExperimentalProbability.UI.Controllers
+{
+    public static class DiceController
+    {
+        public static void UpdateControlsToDice(this MainWindow window)
+        {
+            window.UpdateControlsBasedOnCurrentType(
+                Resources.Text_Pool_Size_Dice,
+                Resources.Default_Pool_Size_Dice,
+                Resources.Text_Condition_Dice_NumberOfTakenItems,
+                Resources.Default_Condition_Dice_NumberOfTakenItems
+            );
+        }
+
+        public static string[] GetDiceSelectionItemsSource(this MainWindow window)
+        {
+            if (int.TryParse(window.Pool_Size.Text, out int numberOfSides)
+                && numberOfSides > 0)
+            {
+                var itemsSource = new List<string>(numberOfSides);
+
+                for (int i = 1; i <= numberOfSides; i++)
+                {
+                    itemsSource.Add(i.ToString());
+                }
+
+                return itemsSource.ToArray();
+            }
+
+            return new string[0];
+        }
+    }
+}
---
+++ ExperimentalProbability.UI/CustomControls/ItemSelectionPanel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ExperimentalProbability.UI.CustomControls
+{
+    public class ItemSelectionPanel : DockPanel
+    {
+        public ItemSelectionPanel(MainWindow window, string text, string[] itemsSource)
+        {
+            DefaultElementSettings();
+            Children.Add(BuildItemSelector(window, text, itemsSource));
+        }
+
+        public ItemSelectionPanel(MainWindow window)
+        {
+            DefaultElementSettings();
+            Children.Add(BuildColorSelector(window));
+            Children.Add(BuildColorText(window));
+            Children.Add(BuildNumberOfColors(window));
+        }
+
+        private void DefaultElementSettings()
+        {
+            LastChildFill = false;
+            Margin = new Thickness(0, 0, 5, 0);
+        }
+
+        private ComboBox BuildItemSelector(MainWindow window, string text, string[] itemsSource)
+        {
+            var selector = GetItemSelector(text, itemsSource);
+
+            return selector;
+        }
+
+        private ComboBox BuildColorSelector(MainWindow window)
+        {
+            var selector = GetItemSelector(Properties.Resources.Default_Selection_ColoredBalls, window.Colors);
+
+            selector.SelectionChanged += new SelectionChangedEventHandler(window.Selection_ColoredBalls_Color_SelectionChanged);
+
+            SetDock(selector, Dock.Top);
+
+            return selector;
+        }
+
+        private ComboBox GetItemSelector(string text, string[] itemsSource)
+        {
+            return new ComboBox()
+            {
+                Margin = new Thickness(0, 0, 0, 5),
+                IsEditable = true,
+                IsReadOnly = true,
+                Text = text,
+                ItemsSource = itemsSource,
+            };
+        }
+
+        private static TextBlock BuildColorText(MainWindow window)
+        {
+            return new TextBlock()
+            {
+                Margin = new Thickness(0, 0, 5, 0),
+                Text = Properties.Resources.Text_ColoredBalls_ColorNumber,
+            };
+        }
+
+        private static TextBox BuildNumberOfColors(MainWindow window)
+        {
+            var textBox = new TextBox()
+            {
+                Width = 30,
+                TextAlignment = TextAlignment.Right,
+                MaxLength = 2,
+                MaxLines = 1,
+                Text = Properties.Resources.Default_ColoredBalls_ColorNumber,
+            };
+
+            SetDock(textBox, Dock.Right);
+
+            return textBox;
+        }
+    }
+}
