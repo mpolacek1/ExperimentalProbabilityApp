ceb6f37c4448444f07bd54e16f945a70ee8e5986
mpolacek1 <matejpolacek1@gmail.com>
2022-02-03 15:37:12 +0100

    misspell in resources corrected

--- ExperimentalProbability.UI/Properties/Types/ColoredBalls/Resources.Designer.cs
+++ ExperimentalProbability.UI/Properties/Types/ColoredBalls/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ExperimentalProbability.UI.Properties.Types.ColoredBalss {
+namespace ExperimentalProbability.UI.Properties.Types.ColoredBalls {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ExperimentalProbability.UI.Properties.Types.ColoredBalss {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ExperimentalProbability.UI.Properties.Types.ColoredBalss.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ExperimentalProbability.UI.Properties.Types.ColoredBalls.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
