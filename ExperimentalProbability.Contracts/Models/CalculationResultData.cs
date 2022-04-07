aba9782d5ee2317536eefdc8bbc2fbf10f5d5e5f
mpolacek1 <matejpolacek1@gmail.com>
2022-04-07 12:44:17 +0200

    reworked calculation data objects

---
+++ ExperimentalProbability.Contracts/Models/CalculationResultData.cs
@@ -0,0 +1,18 @@
+﻿namespace ExperimentalProbability.Contracts.Models
+{
+    public class CalculationResultData
+    {
+        public CalculationResultData()
+        {
+            SimulationsRan = default;
+            ConditionMet = default;
+            Result = default;
+        }
+
+        public int SimulationsRan { get; set; }
+
+        public int ConditionMet { get; set; }
+
+        public decimal Result { get; set; }
+    }
+}
---
+++ ExperimentalProbability.Contracts/Models/Pool/DiceCalculationData.cs
@@ -0,0 +1,16 @@
+﻿namespace ExperimentalProbability.Contracts.Models.Pool
+{
+    public class DiceCalculationData : CalculationData
+    {
+        public DiceCalculationData(int simulationsToRun, int sideCount, int[] conditionSides)
+            : base(simulationsToRun)
+        {
+            SideCount = sideCount;
+            ConditionSides = conditionSides;
+        }
+
+        public int SideCount { get; private set; }
+
+        public int[] ConditionSides { get; private set; }
+    }
+}
