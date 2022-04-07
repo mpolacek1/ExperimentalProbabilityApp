7660c2654feba3732348f3daa3ce2e0a4aa1fe12
mpolacek1 <matejpolacek1@gmail.com>
2022-04-07 12:40:42 +0200

    not needed interfaces removed

--- ExperimentalProbability.UI/Interfaces/ITypeDescription.cs
+++
@@ -1,9 +0,0 @@
-﻿using ExperimentalProbability.UI.Models;
-
-namespace ExperimentalProbability.UI.Interfaces
-{
-    public interface ITypeDescription
-    {
-        void UpdateDescription(DescriptionData typeData, DescriptionData conditionData, char separator = ' ');
-    }
-}
