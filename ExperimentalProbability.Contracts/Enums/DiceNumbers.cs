26f76555b115caae54f0bd9ad5d26bd5058e24dc
mpolacek1 <matejpolacek1@gmail.com>
2022-04-07 12:44:40 +0200

    added enums for integer resource managment in application

---
+++ ExperimentalProbability.Contracts/Enums/DiceNumbers.cs
@@ -0,0 +1,15 @@
+﻿namespace ExperimentalProbability.Contracts.Enums
+{
+    public enum DiceNumbers : int
+    {
+        MinRollResult = 1,
+        SideCountTetrahedron = 4,
+        SideCountCube = 6,
+        SideCountOctahedron = 8,
+        SideCountPentagonalTrapezohedron = 10,
+        SideCountDodecahedron = 12,
+        SideCountIcosahedron = 20,
+        MinNumberOfRolls = 1,
+        MaxNumberOfRolls = 10,
+    }
+}
---
+++ ExperimentalProbability.Contracts/Enums/GeneralNumbers.cs
@@ -0,0 +1,10 @@
+﻿namespace ExperimentalProbability.Contracts.Enums
+{
+    public enum GeneralNumbers : int
+    {
+        MinSimulations = 1000,
+        MaxSimulations = 100000000,
+        MinProgress = 0,
+        MaxProgress = 100,
+    }
+}
