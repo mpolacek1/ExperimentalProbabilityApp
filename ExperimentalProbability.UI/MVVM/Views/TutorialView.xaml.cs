931b37c159db49b72316c7cd1daea86f1582ed24
mpolacek1 <matejpolacek1@gmail.com>
2022-04-07 12:33:27 +0200

    tutorial view redone using mvvm

---
+++ ExperimentalProbability.UI/MVVM/Views/TutorialView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ExperimentalProbability.UI.MVVM.Views
+{
+    public partial class TutorialView : UserControl
+    {
+        public TutorialView()
+        {
+            InitializeComponent();
+        }
+    }
+}
