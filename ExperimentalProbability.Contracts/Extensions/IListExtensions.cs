3d881382821860ec71f285b1cf382002d490f95f
mpolacek1 <matejpolacek1@gmail.com>
2022-04-07 12:43:47 +0200

    added domain extensions

---
+++ ExperimentalProbability.Contracts/Extensions/IListExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+
+namespace ExperimentalProbability.Contracts.Extensions
+{
+    public static class IListExtensions
+    {
+        public static void RewriteOrAddItem(this IList collection, int position, object item)
+        {
+            if (collection.Count >= position + 1)
+            {
+                collection[position] = item;
+            }
+            else
+            {
+                collection.Add(item);
+            }
+        }
+
+        public static void TryAddAndRemove(this IList collection, object itemToAdd, object itemToRemove)
+        {
+            if (itemToAdd != null)
+            {
+                collection.Add(itemToAdd);
+            }
+
+            collection.Remove(itemToRemove);
+        }
+    }
+}
