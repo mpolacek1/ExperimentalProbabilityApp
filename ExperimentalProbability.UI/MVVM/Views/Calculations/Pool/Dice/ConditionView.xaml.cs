652d71a3efca667d9a7678ebbacde8307ff3a865
mpolacek1 <matejpolacek1@gmail.com>
2022-04-07 12:37:25 +0200

    ui for dice calculation redone using mvvm

---
+++ ExperimentalProbability.UI/MVVM/Views/Calculations/Pool/Dice/ConditionView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ExperimentalProbability.UI.MVVM.Views.Calculations.Pool.Dice
+{
+    public partial class ConditionView : UserControl
+    {
+        public ConditionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ExperimentalProbability.UI/MVVM/Views/Calculations/Pool/Dice/PoolView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ExperimentalProbability.UI.MVVM.Views.Calculations.Pool.Dice
+{
+    public partial class PoolView : UserControl
+    {
+        public PoolView()
+        {
+            InitializeComponent();
+        }
+    }
+}
