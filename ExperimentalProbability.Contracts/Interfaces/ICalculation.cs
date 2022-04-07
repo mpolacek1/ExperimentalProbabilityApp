170acf651409c917a96565665d1ae41c3dae6ade
mpolacek1 <matejpolacek1@gmail.com>
2022-04-07 12:47:29 +0200

    moved calculation interface to domain

---
+++ ExperimentalProbability.Contracts/Interfaces/ICalculation.cs
@@ -0,0 +1,14 @@
+﻿using ExperimentalProbability.Contracts.Models;
+using System.ComponentModel;
+
+namespace ExperimentalProbability.Contracts.Interfaces
+{
+    public interface ICalculation
+    {
+        CalculationResultData Run(CalculationData data, DoWorkEventArgs workerEventArgs);
+
+        void ClearResultData();
+
+        decimal CalculateResult(CalculationResultData data);
+    }
+}
