921dfe4d9cd3b313c381f1a5ee6a5c5aefb07eb2
mpolacek1 <matejpolacek1@gmail.com>
2022-04-07 12:32:19 +0200

    main view redone using mvvm

---
+++ ExperimentalProbability.UI/MVVM/Views/ShellView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ExperimentalProbability.UI.MVVM.Views
+{
+    public partial class ShellView : Window
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
