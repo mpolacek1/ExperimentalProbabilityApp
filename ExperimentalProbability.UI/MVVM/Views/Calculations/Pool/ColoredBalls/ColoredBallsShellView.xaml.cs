2804201081fb8a51f8615b4242cd7618ec1a2d16
mpolacek1 <matejpolacek1@gmail.com>
2022-04-07 12:39:00 +0200

    ui for coloredballs calculation redone

---
+++ ExperimentalProbability.UI/MVVM/Views/Calculations/Pool/ColoredBalls/ColoredBallsShellView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ExperimentalProbability.UI.MVVM.Views.Calculations.Pool.ColoredBalls
+{
+    public partial class ColoredBallsShellView : UserControl
+    {
+        public ColoredBallsShellView()
+        {
+            InitializeComponent();
+        }
+    }
+}
