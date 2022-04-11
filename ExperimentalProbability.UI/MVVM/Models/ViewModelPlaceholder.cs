ef6444ab3366378dfb0ef160560ac8b6f4fbcb46
Matěj Poláček <matej.polacek@verisoft.cz>
2022-04-11 17:27:10 +0200

    shell view logic refactored

---
+++ ExperimentalProbability.UI/MVVM/Models/ViewModelPlaceholder.cs
@@ -0,0 +1,39 @@
+﻿using Caliburn.Micro;
+using ExperimentalProbability.UI.MVVM.ViewModels;
+using System;
+
+namespace ExperimentalProbability.UI.MVVM.Models
+{
+    public class ViewModelPlaceholder : PropertyChangedBase
+    {
+        private bool _isShown;
+
+        public ViewModelPlaceholder(string name, bool isVMReference = false, Type type = null, bool isShown = false)
+        {
+            Name = name;
+            IsViewModelReference = isVMReference;
+            Type = type;
+            IsShown = isShown;
+        }
+
+        public string Name { get; private set; }
+
+        public bool IsViewModelReference { get; private set; }
+
+        public Type Type { get; private set; }
+
+        public bool IsShown
+        {
+            get
+            {
+                return _isShown;
+            }
+
+            set
+            {
+                _isShown = value;
+                NotifyOfPropertyChange(() => IsShown);
+            }
+        }
+    }
+}
--- ExperimentalProbability.UI/MVVM/ViewModels/ShellViewModel.cs
+++ ExperimentalProbability.UI/MVVM/ViewModels/ShellViewModel.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Linq;
 using Caliburn.Micro;
+using ExperimentalProbability.Contracts.Properties.Resources.Shell;
+using ExperimentalProbability.UI.MVVM.Models;
 using ExperimentalProbability.UI.MVVM.ViewModels.Calculations.TwoDPlane;
 using ColoredBallsShellVM = ExperimentalProbability.UI.MVVM.ViewModels.Calculations.Pool.ColoredBalls.ShellViewModel;
 using DiceShellVM = ExperimentalProbability.UI.MVVM.ViewModels.Calculations.Pool.Dice.ShellViewModel;
+using PCCBResx = ExperimentalProbability.Contracts.Properties.Resources.Calculations.Pool.ColoredBalls.Resources;
+using PCDResx = ExperimentalProbability.Contracts.Properties.Resources.Calculations.Pool.Dice.Resources;
+using TDPCAResx = ExperimentalProbability.Contracts.Properties.Resources.Calculations.TwoDPlane.Area.Resources;
+using TDPCPResx = ExperimentalProbability.Contracts.Properties.Resources.Calculations.TwoDPlane.Pi.Resources;
+using TResx = ExperimentalProbability.Contracts.Properties.Resources.Tutorial.Resources;
 
 namespace ExperimentalProbability.UI.MVVM.ViewModels
 {
@@ -11,32 +18,24 @@ namespace ExperimentalProbability.UI.MVVM.ViewModels
     {
         public ShellViewModel()
         {
-            ShowTutorial();
+            ViewModelPlaceholders = GenerateVMPlaceholders();
+
+            ActivateWantedVM(ViewModelPlaceholders[0].Type);
         }
 
-        public void ShowTutorial()
-        {
-            ActivateWantedVM(typeof(TutorialViewModel));
-        }
+        public ViewModelPlaceholder[] ViewModelPlaceholders { get; set; }
 
-        public void ShowColoredBallsCalc()
+        public void MenuButtonClick(ViewModelPlaceholder menuItem)
         {
-            ActivateWantedVM(typeof(ColoredBallsShellVM));
-        }
+            foreach (var placeholder in ViewModelPlaceholders)
+            {
+                if (placeholder.Type != null && placeholder != menuItem)
+                {
+                    placeholder.IsShown = false;
+                }
+            }
 
-        public void ShowDiceCalc()
-        {
-            ActivateWantedVM(typeof(DiceShellVM));
-        }
-
-        public void ShowPiCalc()
-        {
-            ActivateWantedVM(typeof(PiShellViewModel));
-        }
-
-        public void ShowAreaCalc()
-        {
-            ActivateWantedVM(typeof(AreaShellViewModel));
+            ActivateWantedVM(menuItem.Type);
         }
 
         public void ActivateWantedVM(Type vm)
@@ -51,6 +50,20 @@ namespace ExperimentalProbability.UI.MVVM.ViewModels
             {
                 ActivateItem(item);
             }
+        }
+
+        private static ViewModelPlaceholder[] GenerateVMPlaceholders()
+        {
+            return new ViewModelPlaceholder[7]
+            {
+                new ViewModelPlaceholder(TResx.DisplayName, true, typeof(TutorialViewModel), true),
+                new ViewModelPlaceholder(Resources.PoolCalcs),
+                new ViewModelPlaceholder(PCCBResx.DisplayName, true, typeof(ColoredBallsShellVM)),
+                new ViewModelPlaceholder(PCDResx.DisplayName, true, typeof(DiceShellVM)),
+                new ViewModelPlaceholder(Resources.TwoDPlaneCalcs),
+                new ViewModelPlaceholder(TDPCPResx.Function, true, typeof(PiShellViewModel)),
+                new ViewModelPlaceholder(TDPCAResx.Function, true, typeof(AreaShellViewModel)),
+            };
         }
     }
 }
